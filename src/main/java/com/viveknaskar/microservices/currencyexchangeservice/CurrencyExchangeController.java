8b2cd759fca1c98b3f4544c082bbf356effe4f5e
Vivek Naskar <viveknaskar2912@gmail.com>
2021-05-08 23:57:42 +0530

    Created CurrencyExchangeController

---
+++ src/main/java/com/viveknaskar/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,21 @@
+package com.viveknaskar.microservices.currencyexchangeservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange retrieveExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to) {
+
+        return new CurrencyExchange(1000L, from, to, BigDecimal.valueOf(50));
+
+    }
+
+}
