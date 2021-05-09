739df1992fa05010e610ef52704171e1127d692f
Vivek Naskar <viveknaskar2912@gmail.com>
2021-05-10 03:24:36 +0530

    Configured JPA and initialized data from data.sql file

--- src/main/java/com/viveknaskar/microservices/currencyexchangeservice/CurrencyExchange.java
+++ src/main/java/com/viveknaskar/microservices/currencyexchangeservice/CurrencyExchange.java
@@ -1,11 +1,22 @@
 package com.viveknaskar.microservices.currencyexchangeservice;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Entity
 public class CurrencyExchange {
+
+    @Id
     private Long id;
+
+    @Column(name = "currency_from")
     private String from;
+
+    @Column(name = "currency_to")
     private String to;
+
     private BigDecimal conversionMultiple;
     private String environment;
 
---
+++ src/main/resources/data.sql
@@ -0,0 +1,9 @@
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10001, 'USD', 'INR', 70, '');
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10002, 'EUR', 'INR', 90, '');
+insert into currency_exchange
+(id, currency_from, currency_to, conversion_multiple, environment)
+values(10003, 'AUD', 'INR', 60, '');
