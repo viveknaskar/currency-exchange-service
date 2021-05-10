8011816f819aded13bdd0dbf060d679393aadc50
Vivek Naskar <viveknaskar2912@gmail.com>
2021-05-10 22:36:53 +0530

    Removed unwanted dependency

--- src/main/java/com/viveknaskar/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/viveknaskar/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -6,8 +6,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.math.BigDecimal;
-
 @RestController
 public class CurrencyExchangeController {
 
