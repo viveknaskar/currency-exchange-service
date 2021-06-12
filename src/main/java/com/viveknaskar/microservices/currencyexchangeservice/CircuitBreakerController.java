eeda13ff46177218fa9028675f168ce74a4a80f2
Vivek Naskar <viveknaskar2912@gmail.com>
2021-06-13 01:32:36 +0530

    Optimized unused imports

--- src/main/java/com/viveknaskar/microservices/currencyexchangeservice/CircuitBreakerController.java
+++ src/main/java/com/viveknaskar/microservices/currencyexchangeservice/CircuitBreakerController.java
@@ -1,14 +1,10 @@
 package com.viveknaskar.microservices.currencyexchangeservice;
 
-import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
-import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
