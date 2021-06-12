47ea3231cc1f11ab1659767c5725578ab8c5f2de
Vivek Naskar <viveknaskar2912@gmail.com>
2021-06-13 01:28:58 +0530

    Added dependencies including sleuth and sleuth-zipkin for distribution tracing in microservices

--- src/main/java/com/viveknaskar/microservices/currencyexchangeservice/CurrencyExchangeController.java
+++ src/main/java/com/viveknaskar/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -1,5 +1,7 @@
 package com.viveknaskar.microservices.currencyexchangeservice;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +10,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CurrencyExchangeController {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(CurrencyExchangeController.class);
 
     @Autowired
     private Environment environment;
@@ -19,6 +23,8 @@ public class CurrencyExchangeController {
     public CurrencyExchange retrieveExchangeValue(
             @PathVariable String from,
             @PathVariable String to) {
+
+        LOGGER.info("retrieveExchangeValue called with {} to  {}", from, to);
 
         CurrencyExchange currencyExchange = repository.findByFromAndTo(from, to);
         if (currencyExchange == null) {
